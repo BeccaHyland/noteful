650237449c928509b7db76383ff01ba2f3b02b30
Rebecca Hyland <39714935+BeccaHyland@users.noreply.github.com>
2018-12-26 15:48:16 -0500

    Update GET lambda function with response header "Access-Control-Allow-Origin"

--- lambda/get_notes.js
+++ lambda/get_notes.js
@@ -1,17 +1,29 @@
 'use strict';
-
+ 
 var AWS = require('aws-sdk'),
-	documentClient = new AWS.DynamoDB.DocumentClient();
-
-exports.readAllNotes = function(event, context, callback){
+	documentClient = new AWS.DynamoDB.DocumentClient(); 
+ 
+exports.readAllNotes = function(event, context, callback) {
 	var params = {
-		TableName : process.env.TABLE_NAME
+		TableName: process.env.TABLE_NAME
 	};
-	documentClient.scan(params, function(err, data){
-		if(err){
+	
+	var cb = function(data) {
+		callback(null, {
+				"data": data.Items,
+		    	"statusCode": 200,
+    			 "headers": { 
+        		 "Access-Control-Allow-Origin": "*" 
+    			}
+		    })
+	}
+	
+	documentClient.scan(params, function(err, data) {
+		if(err) {
 		    callback(err, null);
-		}else{
-		    callback(null, data.Items);
+		} 
+		else {
+			cb(data);
 		}
 	});
 }
