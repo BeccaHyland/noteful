622b7dd0d8120a76eef2060f9bcd17aba9942b64
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-12-26 10:40:55 -0500

    Add spec test for visitor seeing notes index

--- spec/features/visitor_sees_notes_spec.rb
+++ spec/features/visitor_sees_notes_spec.rb
@@ -5,8 +5,17 @@ feature 'visitor sees notes' do
     it 'displays all valid notes from API endpoint' do
       # user story
       # when I visit the root page
+      visit '/' # (to do: after test is functional, stub request with webmock gem so css results are consistent, move API endpoint testing to service spec)
+      expect(page).to have_content("Welcome to Noteful")
+
       # I see a list of all valid notes from the API endpoint
+      expect(page).to have_css(".note", count: 4)
+
       # Each note has a description and a tag
+      within(first(".note")) do
+        expect(page).to have_css(".description")
+        expect(page).to have_css(".tag")
+      end
       # A note is valid when it has 1 of 3 tags "Work", "Personal", "Hobby"
     end
 
