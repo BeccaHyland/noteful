1a346782a92e112cff07ceea8ef4374b147a65f1
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-12-26 10:18:41 -0500

    draft user stories for notes index feature spec

---
+++ spec/features/visitor_sees_notes_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+feature 'visitor sees notes' do
+  describe 'a visitor on the notes index' do
+    it 'displays all valid notes from API endpoint' do
+      # user story
+      # when I visit the root page
+      # I see a list of all valid notes from the API endpoint
+      # Each note has a description and a tag
+      # A note is valid when it has 1 of 3 tags "Work", "Personal", "Hobby"
+    end
+
+    it 'allows visitor to sort notes by tag' do
+      #user story
+      # when I visit the root page
+      # and see all notes
+      # and from the dropdown to sort by tag I select "Hobby"
+      # then I only see notes with the Tag "Hobby"
+    end
+  end
+end
