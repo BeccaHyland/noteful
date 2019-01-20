02ad85734843453a90def2ba537201ae64d64c56
BeccaHyland <rebecca.e.hyland@gmail.com>
2019-01-20 15:02:30 -0700

    test coverage 95%

--- spec/features/visitor_sees_notes_spec.rb
+++ spec/features/visitor_sees_notes_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 feature 'visitor sees notes' do
   describe 'a visitor on the notes index' do
     it 'displays all valid notes from API endpoint' do
-      visit '/' # (to do: after test is functional, stub request with webmock gem so css results are consistent, move API endpoint testing to service spec)
+      visit '/' # (to do: after test is functional, stub request with vcr gem so css results are consistent, move API endpoint testing to service spec)
       expect(page).to have_content("Welcome to Noteful")
 
       expect(page).to have_css(".note")
@@ -30,12 +30,8 @@ feature 'visitor sees notes' do
     end
 
     it 'allows visitor to sort notes by tag' do
-      # when I visit the root page
       visit '/'
-      # and see all notes
-      expect(page).to have_css(".note")
 
-      #create a note with tag Work
       click_on("Add New Note")
       fill_in :description, with: "Study Golang"
       select "Work", from: :tag
@@ -43,7 +39,6 @@ feature 'visitor sees notes' do
       expect(current_path).to eq(notes_path)
       expect(page).to have_content("Study Golang")
 
-      #create a note with tag Hobby
       click_on("Add New Note")
       fill_in :description, with: "Play with kitten"
       select "Hobby", from: :tag
@@ -51,11 +46,9 @@ feature 'visitor sees notes' do
       expect(current_path).to eq(notes_path)
       expect(page).to have_content("Play with kitten")
 
-      # and from the dropdown to sort by tag I select "Hobby"
       select "Hobby", from: :tag
       click_on("Submit Tag")
 
-      # then I only see notes with the Tag "Hobby"
       within(first(".note")) do
         expect(page).to_not have_content("Work")
       end
